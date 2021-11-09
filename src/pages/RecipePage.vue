4bd019f24012ea39091deaf039c5d75bfcf5cd27
raj rana <raj.rana507@gmail.com>
2021-11-09 20:24:52 +0530

    feat: detail page added

--- src/pages/RecipePage.vue
+++ src/pages/RecipePage.vue
@@ -1,16 +1,30 @@
 <template> 
-    <div>
-        this is the recipe detail page
+    <div class="detail">
+        <img :src="details.strMealThumb" alt="" srcset="" class="recipe-image">
+        <div class="p-2">
+            
+            <h3> {{ details.strMeal }} </h3>
+            <p> </p>
+            <p> {{ details.strInstructions }} </p>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
     import { onMounted, ref } from "vue";
     import { useRoute } from "vue-router";
-    import RecipeDataService from "@/services/RecipeService";
+    import RecipeDataService from '@/services/RecipeService';
+
+    interface Recipe { 
+        strMealThumb:string;
+        strMeal:string;   
+        strInstructions:string;
+    }
 
     const route = useRoute();
-    const details = ref({})
+    const details = ref({} as Recipe);
+
+
     onMounted(()=>{
         console.log(route.params.id);
         RecipeDataService.getRecipeDetails(route.params.id).then( (res: any) => { 
@@ -18,10 +32,34 @@
                 details.value = res.data.meals[0]
             }
 
-            console.log("details ....", details.value);
         })
     })
 
 </script>
 
-<style scoped></style>
+<style scoped>
+ .detail { 
+     padding: 1rem;
+     display: flex;
+     justify-content: space-around;
+ }
+
+ .recipe-image { 
+     height: 500px;
+     width: 600px;
+     object-fit: contain;
+ }
+
+@media only screen and (max-width: 600px) {
+    .detail {
+        flex-direction: column;
+    }
+
+    
+ .recipe-image { 
+     height: 400px;
+     width: 100%;
+     object-fit: cover;
+ }
+}
+</style>
